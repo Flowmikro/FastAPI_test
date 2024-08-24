d3e1d315d51a4b524932f00b72ce0390a1d78fe9
Артур Лечоев <islam.lechoev@gdl-it.com>
2024-08-25 00:14:58 +0300

    Подключение роутера

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import contextlib
+from typing import AsyncIterator
+
 from fastapi import FastAPI
 
+import app_settings
+from game.routers import router as game_router
 
-app = FastAPI()
 
+@contextlib.asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncIterator[None]:
+    app_settings.db_manager.init(app_settings.settings.database_url)
+    yield
+    await app_settings.db_manager.close()
+
+
+app = FastAPI(title="GameAPI", lifespan=lifespan)
+app.include_router(game_router, prefix='/api/game', tags=['Game'])
