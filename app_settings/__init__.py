6892f1e6c3145b97cf975ca1186a740e7427fd91
Артур Лечоев <islam.lechoev@gdl-it.com>
2024-08-24 18:40:20 +0300

    Настойка проекта и подключение Алхимии

---
+++ app_settings/__init__.py
@@ -0,0 +1,10 @@
+from .base import Base
+from .config import settings
+from .session_manager import db_manager, get_session
+
+__all__ = [
+    "Base",
+    "get_session",
+    "db_manager",
+    "settings",
+]
